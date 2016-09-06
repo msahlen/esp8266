8ce5cffa47094c3cc474fc358eee38fab3337522
Mattias Sahlén <mattias.sahlen@vinnter.se>
2016-09-06 16:22:05 +0200

    Changed GPIO pins since the trigger did not work

--- wlm/wlm.c
+++ wlm/wlm.c
@@ -3,49 +3,78 @@
 #include "gpio.h"
 #include "os_type.h"
 
-static const int led_pin  = 2;
-static const int trig_pin = 12;
-static const int echo_pin = 13;
+static const int led_pin  = 13;
+static const int trig_pin = 5;
+static const int echo_pin = 4;
+static const int ping_trigger_length = 10000;
+
 static volatile os_timer_t blink_timer;
 
-void timerfunc(void *arg)
+static void trigger(void)
 {
-  //Do blinky stuff
-  if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & (1 << led_pin))
-  {
-    // set gpio low
-    gpio_output_set(0, (1 << led_pin), 0, 0);
-  }
-  else
-  {
-    // set gpio high
-    gpio_output_set((1 << led_pin), 0, 0, 0);
-  }
+    os_printf("Trigger\n");    
+    // set high, wait 10 ms, the set low again
+    gpio_output_set((1 << trig_pin), 0, 0, 0);
+    os_delay_us(ping_trigger_length);
+    gpio_output_set(0, (1 << trig_pin), 0, 0);
 }
 
-void setup_gpios(void)
+void timerfunc(void *arg)
+{  
+    //Do blinky stuff
+    if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & (1 << led_pin))
+    {
+      // set low
+      os_printf("Set low\n");
+      gpio_output_set(0, (1 << led_pin), 0, 0);
+    }
+    else
+    {
+      // set high
+      os_printf("Set high\n");
+      gpio_output_set((1 << led_pin), 0, 0, 0);
+    }
+
+    trigger();
+}
+
+static void echo_handler(uint32 mask, void *arg) 
 {
-  // init gpio subsystem
-  gpio_init();
+    os_printf("Echo received\n");     
+    uint32 gpio_status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
+    os_printf("status: 0x%x\n", gpio_status);
+    gpio_intr_ack(mask);
+}
+
+void ICACHE_FLASH_ATTR setup_gpios(void)
+{
+    // init gpio subsystem
+    gpio_init();
   
-  // Configure GPIO pins
-  PIN_FUNC_SELECT(PERIPHS_IO_MUX_GPIO2_U, FUNC_GPIO2);
-  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_GPIO12);
-  PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13);
+    // Configure GPIO pins
+    PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_GPIO13);
   
-  gpio_output_set(0, 0, (1 << led_pin) | (1 << trig_pin),(1 << echo_pin));        
+    gpio_output_set(0, 0, (1 << led_pin) | (1 << trig_pin),(1 << echo_pin));        
+}
+
+void ICACHE_FLASH_ATTR setup_interrupts(void)
+{
+    os_printf("Setup interrupts\n");    
+    gpio_intr_handler_register(echo_handler,NULL);
+    gpio_pin_intr_state_set(GPIO_ID_PIN(echo_pin), GPIO_PIN_INTR_POSEDGE);
 }
 
 void ICACHE_FLASH_ATTR user_init()
 {
-  // Set baud rate      
-  uart_div_modify(0, UART_CLK_FREQ / 115200);      
-  os_printf("SDK version:%s\n", system_get_sdk_version());
-  os_printf("Module Test\nChip_id: %lu\n", system_get_chip_id());
+    // Set baud rate      
+    uart_div_modify(0, UART_CLK_FREQ / 115200);      
+    os_printf("SDK version:%s\n", system_get_sdk_version());
+    os_printf("Module Test\nChip_id: %lu\n", system_get_chip_id());
   
-  setup_gpios();
-
-  // setup timer (500ms, repeating)
-  os_timer_setfn(&blink_timer, (os_timer_func_t *)timerfunc, NULL);
-  os_timer_arm(&blink_timer, 500, 1);
+    setup_gpios();
+    //setup_interrupts();
+    
+    // setup timer (500ms, repeating)
+    os_timer_setfn(&blink_timer, (os_timer_func_t *)timerfunc, NULL);
+    os_timer_arm(&blink_timer, 500, 1);
 }
